45c50f97d37a55ef109588708e72844c3fff57b2
AndyGuit <totalslash@msn.com>
2024-03-22 14:50:30 +0200

    refactor: garage view class

--- async-race/src/views/Garage/GarageView.ts
+++ async-race/src/views/Garage/GarageView.ts
@@ -1,9 +1,9 @@
 import './GarageView.css';
 import Button from '../../components/Button/Button';
 import Input from '../../components/Input/Input';
-import { getAllWinners } from '../../api/WinnersApi';
 import { getAllCars } from '../../api/CarsApi';
 import CarItem from '../../components/CarItem/CarItem';
+import { ICarResponseData } from '../../types/interfaces';
 
 export default class GarageView {
   private element: HTMLElement;
@@ -70,9 +70,12 @@ export default class GarageView {
   }
 
   async init() {
+    const { totalCars, cars } = await getAllCars(1);
+
     this.renderControls();
 
-    await this.renderCarsList();
+    this.renderHeadings(totalCars);
+    this.renderCarsList(cars);
   }
 
   getElement() {
@@ -89,11 +92,7 @@ export default class GarageView {
     this.element.append(h1, h2);
   }
 
-  async renderCarsList() {
-    const { totalCars, cars } = await getAllCars(1);
-
-    this.renderHeadings(totalCars);
-
+  renderCarsList(cars: ICarResponseData[]) {
     const ul = document.createElement('ul');
     ul.classList.add('cars-list');
 
@@ -102,28 +101,5 @@ export default class GarageView {
     ul.append(...carItems);
 
     this.element.append(ul);
-  }
-
-  async fetchCars() {
-    // const updatedCar = await updateCar(5, { name: 'Updated', color: '#000000' });
-    // const cars = await getAllCars(1);
-    // const car = await getCar(4);
-    // createWinner({ id: 2, time: 100, wins: 1 });
-    // updateWinner({ id: 2, time: 200, wins: 2 });
-    const winners = await getAllWinners();
-    // const winner = await getWinner(1);
-
-    // createWinner({ id: 2, time: 500, wins: 4 });
-
-    // createCar({ name: 'car', color: '#ff00ff' });
-    // const del = await deleteCar(6);
-    // console.log(del);
-
-    // console.log('updated: ', updatedCar);
-    // console.log('cars: ', cars);
-    // console.log('car: ', car);
-
-    console.log('winners: ', winners);
-    // console.log('winner: ', winner);
   }
 }
