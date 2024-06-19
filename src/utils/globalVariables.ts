38301cf2e1e7ef58cbf604b03ae3144764277bbd
AndyGuit <totalslash@msn.com>
2024-06-19 17:19:19 +0300

    feat: update api url link

--- src/utils/globalVariables.ts
+++ src/utils/globalVariables.ts
@@ -1,3 +1,3 @@
-export const API_URL = 'http://127.0.0.1:3000';
+export const API_URL = 'https://async-race-api-2a6e.onrender.com'; // 'http://127.0.0.1:3000';
 export const LIMIT_CARS_PER_PAGE = 7;
 export const LIMIT_WINNERS_PER_PAGE = 10;
